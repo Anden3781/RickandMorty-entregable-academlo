c988d48c4cc35b78789a69e84dfa381c15611aab
Anderson Puppi <anderson_samyr@hotmail.com>
2023-03-19 20:02:22 -0500

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,102 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { getRandomNumber } from './utils/getRandomNumber';
-import Location from './components/location';
+import React, { useEffect, useState } from 'react';
+import Location from './components/Location';
 import ResidentList from './components/ResidentList';
+import { getRandomNumber } from './utils/getRandomNumber';
+import banner from '/banner.png';
 
 const App = () => {
-  const [locateInfo, setLocateInfo] = useState(null);
+  const [locationInfo, setLocationInfo] = useState(null);
   const [idLocationValue, setIdLocationValue] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
-  // location random
-  const idLocationRandom = () => getRandomNumber(1, 126);
+  const getIdLocationRandom = () => getRandomNumber(1, 126);
 
   const loadLocationInfo = async (idLocation) => {
-    const url = `https://rickandmortyapi.com/api/location/${idLocationRandom()}`;
+    const url = `https://rickandmortyapi.com/api/location/${idLocation}`;
 
     try {
       const res = await axios.get(url);
-
-      setLocateInfo(res.data);
+      setLocationInfo(res.data);
     } catch (error) {
       console.log(error);
     }
   };
-  // Input numerico controlado
+
+  const loadSuggestions = async (searchTerm) => {
+    const url = `https://rickandmortyapi.com/api/location/?name=${searchTerm}`;
+
+    try {
+      const res = await axios.get(url);
+      setSuggestions(res.data.results);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const idLocationHandleChange = (e) => {
     const newValue = e.target.value;
 
-    if (/^\d{0,3}$/.test(newValue)) setIdLocationValue(newValue);
+    if (/^[a-zA-Z\d]{0,3}$/.test(newValue)) setIdLocationValue(newValue);
+    loadSuggestions(newValue);
+  };
+
+  const handleSuggestionClick = (id) => {
+    setSuggestions([]);
+    setIdLocationValue(id);
+    loadLocationInfo(id);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (idLocationValue) loadLocationInfo(idLocationValue);
-    else loadLocationInfo(idLocationRandom());
+    else loadLocationInfo(getIdLocationRandom());
   };
 
   useEffect(() => {
-    loadLocationInfo(idLocationRandom());
+    loadLocationInfo(getIdLocationRandom());
   }, []);
 
   return (
-    <div className="bg-neutral-800 h-screen flex flex-col justify-center items-center p-10 text-white">
-      <form onSubmit={handleSubmit}>
-        <input
-          className="text-black"
-          type="search"
-          name="id-location"
-          value={idLocationValue}
-          onChange={idLocationHandleChange}
-          placeholder="1-126"
-        />
-        <input type="submit" value="Search" />
+    <div className="bg-teal-800 h-min-full flex flex-col justify-center items-center text-white pb-6">
+      <div>
+        <img className="w-min-full" src={banner} alt="banner" />
+      </div>
+      <form className="my-10 w-10/12 flex" onSubmit={handleSubmit}>
+        <div className="relative flex-1">
+          <input
+            className="text-white bg-teal-900 w-full p-3 pl-10 rounded-tl rounded-bl"
+            type="search"
+            name="id-location"
+            value={idLocationValue}
+            onChange={idLocationHandleChange}
+            placeholder="Type a location name"
+          />
+          {suggestions.length > 0 && (
+            <ul className="absolute z-10 bg-black w-full mt-1 rounded-b-md max-h-40 overflow-y-scroll">
+              {suggestions.map((suggestion) => (
+                <li
+                  key={suggestion.id}
+                  className="px-4 py-2 hover:text-black hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handleSuggestionClick(suggestion.id)}
+                >
+                  {suggestion.name}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <button
+          type="submit"
+          className="font-semibold cursor-pointer bg-teal-700 text-white py-3 px-4 hover:bg-teal-600 rounded-r-md w-auto"
+        >
+          Search
+        </button>
       </form>
 
-      {locateInfo && <Location {...locateInfo} />}
-      {locateInfo && <ResidentList residents={locateInfo.residents} />}
+      {locationInfo && <Location {...locationInfo} />}
+      {locationInfo && <ResidentList residents={locationInfo.residents} />}
     </div>
   );
 };
--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 const ResidentInfo = ({ urlResident }) => {
   const [residentInfo, setResidentInfo] = useState(null);
 
   const loadResidentInfo = async () => {
     try {
-      // Peticion de la informacion del residente a travez de una prop
       const res = await axios.get(urlResident);
 
       setResidentInfo(res.data);
@@ -20,32 +19,42 @@ const ResidentInfo = ({ urlResident }) => {
   }, []);
 
   return (
-    // renderizado condicional cada vez que residentInfo exista, renderiza.
     <>
       {residentInfo && (
-        <article>
-          <div>
-            <img src={residentInfo.image} alt={residentInfo.name} />
+        <article className="relative h-full">
+          <img className="rounded-tl-lg rounded-tr-lg" src={residentInfo.image} alt="" />
+          <div className="bg-teal-900 rounded-bl-lg rounded-br-lg p-2">
+            <h3 className="font-bold text-2xl border-b border-teal-400 mb-2 pl-1 pb-2">
+              {residentInfo.name}
+            </h3>
+            <ul className="pl-2">
+              <li className="pb-1 font-bold text-xl">
+                <span className="text-base text-gray-400">SPECIE: </span>
+                <br />
+                {residentInfo.species}
+              </li>
+              <li className="absolute top-4 left-0 bg-teal-900 p-1 px-4 rounded-tr-xl rounded-br-xl">
+                <span>
+                  {residentInfo.status === 'Alive'
+                    ? '🟢 '
+                    : residentInfo.status === 'Dead'
+                    ? '🔴 '
+                    : '🔘 '}
+                </span>
+                {residentInfo.status}
+              </li>
+              <li className="pb-1 font-bold text-xl">
+                <span className="text-base text-gray-400">ORIGEN: </span>
+                <br />
+                {residentInfo.origin.name}
+              </li>
+              <li className="pb-2 font-bold text-xl">
+                <span className="text-base text-gray-400">EPISODE APPEARANCES: </span>
+                <br />
+                {residentInfo.episode.length}
+              </li>
+            </ul>
           </div>
-          <h3>{residentInfo.name}</h3>
-          <ul>
-            <li>
-              <span>specie:</span>
-              {residentInfo.species}
-            </li>
-            <li>
-              <span>Status</span>
-              {residentInfo.status}
-            </li>
-            <li>
-              <span>Origen</span>
-              {residentInfo.origin.name}
-            </li>
-            <li>
-              <span>Appearances in episodes:</span>
-              {residentInfo.episode.length}
-            </li>
-          </ul>
         </article>
       )}
     </>
